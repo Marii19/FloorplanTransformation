ad0f917017cfc16331bd3eee3d53be6280c4864d
Trzeciakiewicz <treciakiewicz@fe.hhi.de>
2023-02-24 10:54:53 +0100

    automatic push

--- apptainer/train.sh
+++ apptainer/train.sh
@@ -1,4 +1,5 @@
 ls /mnt/datasets/cubicasa -l
+pip show
 nvidia-smi 
-pip install tensorflow==2.8.0 
+
 python /mnt/code/pytorch/train.py --datasetPath /mnt/datasets/cubicasa/ --batchSize 1 --numTrainingImages 8 --restore 0
