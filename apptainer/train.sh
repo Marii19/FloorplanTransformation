06ccde76e527365c42c76cf0dec89a195bd50689
Trzeciakiewicz <treciakiewicz@fe.hhi.de>
2023-02-24 11:50:07 +0100

    automatic push

--- apptainer/train.sh
+++ apptainer/train.sh
@@ -2,4 +2,4 @@ ls /mnt/datasets/cubicasa -l
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 nvidia-smi 
 
-python /mnt/code/pytorch/train.py --datasetPath /mnt/datasets/cubicasa/ --batchSize 1 --numTrainingImages 8 --restore 0
+#python /mnt/code/pytorch/train.py --datasetPath /mnt/datasets/cubicasa/ --batchSize 1 --numTrainingImages 8 --restore 0
